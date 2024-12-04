6afd700f7324b27ca8490ed187ae24f435d8d6f6
CristianGC006 <cogolloguzmanesteban@gmail.com>
2024-12-04 11:23:42 -0500

    first commit

---
+++ view/public/js/main.js
@@ -0,0 +1,26 @@
+//Boton de Preguntas frecuentes
+document.querySelector('.botonPreguntas').onclick = function() {
+    window,location.href="view/public/pages/preguntas.html";
+};
+
+document.querySelector('.botonInicio').onclick = function() {
+    window,location.href='../index.html';
+};
+
+//Boton de Formulario de inicio de seccion
+document.querySelector('.botonIngresar').onclick = function() {
+    window,location.href="view/public/pages/login.html";
+};
+//Boton de Formulario de Registro
+document.querySelector('.botonHazteCliente').onclick = function() {
+    window,location.href="view/public/pages/registro.html";
+};
+
+document.querySelector('.botonHazteCliente2').onclick = function() {
+    window,location.href="view/public/pages/registro.html";
+};
+
+//Boton de Formulario de Registro de Empresas
+document.querySelector('.botonEmpresas').onclick = function() {
+    window,location.href="view/public/pages/empresas.html";
+};
---
+++ view/public/js/preguntas.js
@@ -0,0 +1,17 @@
+//Boton de Preguntas frecuentes
+document.querySelector('.botonPreguntas').onclick = function() {
+    window,location.href="preguntas.html";
+};
+
+//Boton de Formulario de inicio de seccion
+document.querySelector('.botonIngresar').onclick = function() {
+    window,location.href="login.html";
+};
+//Boton de Formulario de Registro
+document.querySelector('.botonHazteCliente').onclick = function() {
+    window,location.href="registro.html";
+};
+
+document.querySelector('.botonHazteCliente2').onclick = function() {
+    window,location.href="registro.html";
+};
